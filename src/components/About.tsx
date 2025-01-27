d419bd79da8cbb24658665fe8ea3018a48c8495b
Tommi Heinonen <tommi.heinonen@student.lut.fi>
2025-01-27 18:31:28 +0200

    useEffect fetching data and additional translations

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from 'react'
+
 
 const About = () => {
+    interface Post {
+        id: number;
+        title: string;
+        body: string;
+    }
+
+    const [posts, setPosts] = useState<Post[]>([])
+
+useEffect(() => { 
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => setPosts(data))
+}, [])
+
+
   return (
     <div>
       <h2>About</h2>
+      {posts.map(post => (
+            <div key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+            </div>
+      ))}
     </div>
   )
 }
--- src/components/MyContainer.tsx
+++ src/components/MyContainer.tsx
@@ -1,9 +1,10 @@
-
+import { useTranslation } from "react-i18next"
 
 const MyContainer = () => {
+  const { t } = useTranslation()
   return (
     <div>
-      <h2>My Container</h2>
+      <h2>{t('This is the front page')}</h2>
     </div>
   )
 }
