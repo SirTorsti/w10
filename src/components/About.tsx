da8fc5d8c9854d43eccaed5907f9777a43968518
Tommi Heinonen <tommi.heinonen@student.lut.fi>
2025-01-27 18:59:08 +0200

    added pagination

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,6 +9,7 @@ const About = () => {
     }
 
     const [posts, setPosts] = useState<Post[]>([])
+    const [visibleItems, setVisibleItems] = useState<number>(12)
 
 useEffect(() => { 
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -16,18 +17,25 @@ useEffect(() => {
     .then(data => setPosts(data))
 }, [])
 
+const showMoreItems = () => {
+  setVisibleItems(prevVisibleItems => prevVisibleItems + 12)
+}
+
 
   return (
     <div>
       <h2>About</h2>
       <div className="grid-container">
-        {posts.map(post => (
+        {posts.slice(0, visibleItems).map(post => (
               <div key={post.id} className="grid-item">
                   <h3>{post.title}</h3>
                   <p>{post.body}</p>
               </div>
         ))}
       </div>
+      {visibleItems < posts.length && (
+        <button onClick={showMoreItems}>Show more</button>
+      )}
     </div>
   )
 }
