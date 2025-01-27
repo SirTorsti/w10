4014ddeefdbb04b42bde6161445e33b16d85a5ab
Tommi Heinonen <tommi.heinonen@student.lut.fi>
2025-01-27 17:30:54 +0200

    added i18 config, imports etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import About from './components/About'
 import Header from './components/Header'
@@ -9,7 +7,7 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
-import { Suspense } from 'react'
-import './styles/Header.css'
+import '../styles/Header.css'
 
 const Header = () => {
     const { t, i18n } = useTranslation()
@@ -13,9 +11,9 @@ const Header = () => {
     <header className = "header">
       <h1>My App</h1>
       <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
+        <ul className = "nav-list">
+            <li><a href="/">{t('Home')}</a></li>
+            <li><a href="/about">{t('About')}</a></li>
             <button onClick={() => changeLanguage("fi")}>FI</button>
             <button onClick={() => changeLanguage("en")}>EN</button>
         </ul>
@@ -24,10 +22,4 @@ const Header = () => {
   )
 }
 
-export default function App() {
-  return (
-    <Suspense fallback="loading...">
-      <Header />
-    </Suspense>
-  )
-}
+export default Header
